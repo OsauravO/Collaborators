5292f7a6bfa714564cb07dba34687a11c5524480
OsauravO <saurav1hasyo@gmail.com>
2024-09-20 22:26:25 +0530

    fix: origin link

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:3001", "https://storied-sprinkles-5a102f.netlify.app"],
+    origin: ["http://localhost:3000", "http://localhost:3001", "https://escanor-task-manager.netlify.app"],
     methods: ["GET", "POST", "DELETE", "PUT"],
     credentials: true,
   })
