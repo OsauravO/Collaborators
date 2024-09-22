f26f7b6789e564a1aebb56be6b387c22f71d2943
OsauravO <saurav1hasyo@gmail.com>
2024-09-22 11:01:12 +0530

    fix: routing fixed

--- server/routes/taskRoutes.js
+++ server/routes/taskRoutes.js
@@ -15,20 +15,20 @@ import { isAdminRoute, protectRoute } from "../middlewares/authMiddlewave.js";
 
 const router = express.Router();
 
-router.post("/api/task/create", protectRoute, isAdminRoute, createTask);
-router.post("/api/task/duplicate/:id", protectRoute, isAdminRoute, duplicateTask);
-router.post("/api/task/activity/:id", protectRoute, postTaskActivity);
+router.post("/create", protectRoute, isAdminRoute, createTask);
+router.post("/duplicate/:id", protectRoute, isAdminRoute, duplicateTask);
+router.post("/activity/:id", protectRoute, postTaskActivity);
 
-router.get("/api/task/dashboard", protectRoute, dashboardStatistics);
-router.get("/api/task/", protectRoute, getTasks);
-router.get("/api/task/:id", protectRoute, getTask);
+router.get("/dashboard", protectRoute, dashboardStatistics);
+router.get("/", protectRoute, getTasks);
+router.get("/:id", protectRoute, getTask);
 
-router.put("/api/task/create-subtask/:id", protectRoute, isAdminRoute, createSubTask);
-router.put("/api/task/update/:id", protectRoute, isAdminRoute, updateTask);
-router.put("/api/task/:id", protectRoute, isAdminRoute, trashTask);
+router.put("/create-subtask/:id", protectRoute, isAdminRoute, createSubTask);
+router.put("/update/:id", protectRoute, isAdminRoute, updateTask);
+router.put("/:id", protectRoute, isAdminRoute, trashTask);
 
 router.delete(
-  "api/task/delete-restore/:id?",
+  "/delete-restore/:id?",
   protectRoute,
   isAdminRoute,
   deleteRestoreTask
